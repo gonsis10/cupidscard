7b9ae1fd12d174f58196ee7bb6df2fbbc298dbf3
Simoon Song <simoonsong1006@gmail.com>
2025-02-09 19:45:20 -0800

    tweeks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+	...compat.config({
+		extends: ["next/core-web-vitals"],
+	}),
+];
 
 export default eslintConfig;
